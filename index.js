bd55970fb446e7ad62720764c388aaae16ca2cd7
Krum Kuzmov <kkuzmov@gmail.com>
2021-01-27 23:15:27 +0200

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const config = require('./config/config');
+
+console.log(process.env.NODE_ENV);
+
+app.listen(config.PORT, ()=>{
+    console.log(`Server is running on port ${config.PORT}`)
+});
