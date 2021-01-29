749e0c4b75fb37facbb9430ac898340dbf5b6e38
Krum Kuzmov <kkuzmov@gmail.com>
2021-01-29 12:04:52 +0200

    create save to products functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,20 +1,26 @@
-const { Router } = require('express');
-
+const {Router} = require('express');
 const router = Router();
-
-
-router.get('/', (req, res)=>{
-    res.render('home', {title: 'Home'});
+const productService = require('../services/productService');
+router.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Home'
+    });
 })
-router.get('/create', (req, res)=>{
-    res.render('create', {title: 'Create a cube'});
+router.get('/create', (req, res) => {
+    res.render('create', {
+        title: 'Create a cube'
+    });
 })
-router.post('/create', (req, res)=>{
+router.post('/create', (req, res) => {
+    productService.createCube(req.body);    
+
     res.redirect('/products')
 })
-router.get('/details/:productId', (req, res)=>{
+router.get('/details/:productId', (req, res) => {
     console.log(req.params.productId);
-    res.render('details', {title: 'Details'});
+    res.render('details', {
+        title: 'Details'
+    });
 })
 
 module.exports = router;
---
+++ services/productService.js
@@ -0,0 +1,26 @@
+const uniqid = require('uniqid');
+const Cube = require('../models/cube');
+const productsData = require('../config/products.json')
+const fs = require('fs');
+
+function createCube(data){
+    let cube = new Cube(
+        uniqid(),
+        data.name,
+        data.description,
+        data.imageUrl,
+        data.difficultyLevel
+        );
+    
+    productsData.push(cube);
+
+    fs.writeFile(__dirname + '/../config/products.json', JSON.stringify(productsData), (err)=>{
+        if(err){
+            console.log(err);
+            return;
+        }
+    })
+}
+module.exports = {
+    createCube
+}
